ab29cc5cdc09596e340f466e1404616f0a220cbd
WolfieLeader <koren2318@gmail.com>
2022-10-15 15:20:21 +0300

    Defualt export working on commonJs as well

--- api/app.ts
+++
@@ -1,8 +0,0 @@
-import express, { Application } from "express";
-
-const app: Application = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-export default app;

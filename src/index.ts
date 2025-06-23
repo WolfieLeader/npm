37d48e586db2cac76166e88596dbeebeeb75896f
WolfieLeader <koren2318@gmail.com>
2025-06-23 19:05:58 +0300

    BREAKING CHANGE: rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,87 @@
-import { isIP } from "net";
-import { Request } from "express";
+import { isIP } from 'node:net';
+import type { NextFunction, Request, Response } from 'express';
 
-const isValidIP = (ip: unknown): boolean => {
-  if (!ip || typeof ip !== "string") return false;
-  if (isIP(ip) === 4 || isIP(ip) === 6) return true;
-  return false;
-};
+const $isIP = (ip: unknown): ip is string => typeof ip === 'string' && isIP(ip) !== 0;
 
-const getIPFromHeaders = (req: Request): string[] | null => {
-  const potentialHeaders = [
-    "x-client-ip",
-    "x-forwarded-for",
-    "forwarded-for",
-    "x-forwarded",
-    "x-real-ip",
-    "cf-connecting-ip",
-    "true-client-ip",
-    "x-cluster-client-ip",
-    "fastly-client-ip",
-    "x-appengine-user-ip",
-    "Cf-Pseudo-IPv4",
-  ];
+const headers = [
+  'x-client-ip',
+  'x-forwarded-for',
+  'forwarded-for',
+  'x-forwarded',
+  'x-real-ip',
+  'cf-connecting-ip',
+  'true-client-ip',
+  'x-cluster-client-ip',
+  'fastly-client-ip',
+  'x-appengine-user-ip',
+  'Cf-Pseudo-IPv4',
+];
+
+function $extractIpFromHeaders(req: Request): [string, ...string[]] | null {
+  if ($isIP(req.ip)) return [req.ip];
+
   if (!req.headers) return null;
-  if (req.headers.forwarded && isValidIP(req.headers.forwarded)) return [req.headers.forwarded];
-  for (const header of potentialHeaders) {
-    const ipOrIps = req.headers[header];
-    if (ipOrIps) {
-      if (Array.isArray(ipOrIps)) {
-        const filteredIps = ipOrIps.filter((ip) => isValidIP(ip));
-        if (filteredIps.length > 0) return filteredIps;
-      }
-      if (typeof ipOrIps === "string") {
-        if (ipOrIps.includes(",")) {
-          const filteredIps = ipOrIps.split(",").filter((ip) => isValidIP(ip.trim()));
-          if (filteredIps.length > 0) return filteredIps;
-        }
-        if (isValidIP(ipOrIps)) return [ipOrIps];
+  if ($isIP(req.headers.forwarded)) return [req.headers.forwarded];
+
+  for (let i = 0; i < headers.length; i++) {
+    const ip = req.headers[headers[i] as string];
+    if (!ip) continue;
+    if (Array.isArray(ip)) {
+      const filteredIps = ip.filter((item) => $isIP(item.trim()));
+      if (filteredIps.length > 0) return filteredIps.map((item) => item.trim()) as [string, ...string[]];
+    }
+
+    if (typeof ip === 'string') {
+      if ($isIP(ip.trim())) return [ip.trim()];
+      if (!ip.includes(',')) continue;
+      const filteredIps = ip.split(',').filter((ip) => $isIP(ip.trim()));
+      if (filteredIps.length > 0) {
+        return filteredIps.map((item) => item.trim()) as [string, ...string[]];
       }
     }
   }
   return null;
-};
+}
 
-const getClientIp = (req: Request): string | null => {
-  if (!req) throw new Error("Request is undefined");
-  const ips = getIPFromHeaders(req);
-  if (ips && ips.length > 0) return ips[0];
-  if (req.socket && req.socket.remoteAddress && isValidIP(req.socket.remoteAddress)) return req.socket.remoteAddress;
-  if (req.connection && req.connection.remoteAddress && isValidIP(req.connection.remoteAddress))
-    return req.connection.remoteAddress;
-  return null;
-};
+/**
+ * Middleware to extract the client IP address from the request.
+ * This function checks various headers and the request socket to find the client's IP address.
+ * It sets `req.clientIp` to the first valid IP address found and `req.clientIps` to an array of all valid IPs.
+ * If no valid IP is found, these properties will not be set.
+ */
+export function getClientIp(req: Request, response: Response, next?: NextFunction) {
+  if (!req) throw new Error('Request is undefined');
 
-export default getClientIp;
+  const ips = $extractIpFromHeaders(req);
+  if (ips && ips.length > 0) {
+    req.clientIp = ips[0];
+    req.clientIps = ips;
+    if (next) next();
+    return;
+  }
+
+  if ($isIP(req.socket.remoteAddress)) {
+    req.clientIp = req.socket.remoteAddress;
+    req.clientIps = [req.socket.remoteAddress];
+    if (next) next();
+    return;
+  }
+
+  if ($isIP(req.connection.remoteAddress)) {
+    req.clientIp = req.connection.remoteAddress;
+    req.clientIps = [req.connection.remoteAddress];
+    if (next) next();
+    return;
+  }
+}
+
+declare global {
+  namespace Express {
+    export interface Request {
+      /** The first IP address extracted from the request headers */
+      clientIp?: string;
+      /** The array of all IP addresses extracted from the request headers */
+      clientIps?: [string, ...string[]];
+    }
+  }
+}
---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { type Options, defineConfig } from 'tsup';
+
+export default defineConfig((options: Options) => ({
+  name: 'get-client-ip',
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['cjs', 'esm'],
+  clean: true,
+  dts: true,
+  sourcemap: true,
+  treeshake: true,
+  splitting: true,
+  minify: false,
+  cjsInterop: true,
+  tsconfig: 'tsconfig.json',
+  skipNodeModulesBundle: true,
+  external: ['express'],
+  ...options,
+}));

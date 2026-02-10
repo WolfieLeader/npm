7e1c56b254b4ab84d7325e5e9330ae6ed27436ab
WolfieLeader <koren2318@gmail.com>
2026-02-10 14:33:46 +0200

    chore: remove examples

--- examples/server-nestjs/src/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from "@nestjs/common";
-import { EnvModule } from "./modules/env.module";
-import { RoutesModule } from "./modules/routes/routes.module";
-
-@Module({ imports: [EnvModule, RoutesModule] })
-export class AppModule {}
--- examples/server-nestjs/src/modules/routes/routes.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from "@nestjs/common";
-import { RoutesController } from "./routes.controller";
-
-@Module({ controllers: [RoutesController] })
-export class RoutesModule {}
